fce4821b8a473697e2cfa11b839337c8e6ad7326
Joel Day <joel.day@accretivetg.com>
2018-08-26 14:45:07 -0700

    Adding --yes to lerna publish.

--- build/ci-publish.sh
+++ build/ci-publish.sh
@@ -9,19 +9,19 @@ set -e
 
 echo Publishing packages...
 
-git checkout master
 git add .
 git reset --hard
+git checkout master
 
-lerna publish prerelease --preid preview
+lerna publish prerelease --preid preview --yes
 
 echo Publishing Visual Studio Code extension package...
 pushd ./ > /dev/null
 cd packages/papyrus-lang-vscode
 
 rm -rf node_modules
-
 npm install
+
 vsce package
 vsce publish
 
