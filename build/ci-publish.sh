8e2f4323d752034d39b6b5edd541b609bd7c30e6
Joel Day <joel.day@accretivetg.com>
2018-08-26 14:25:25 -0700

    Checking out master in ci publish script.

--- build/ci-publish.sh
+++ build/ci-publish.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ -z "$CI" ]; then
+    echo "Error: Publish script can only run in a CI environment."
+    exit 1
+fi
+
 set -e
 
 echo Publishing packages...
+
+git checkout master
 lerna publish prerelease --preid preview
 
 echo Publishing Visual Studio Code extension package...
