1b76ec1ae7aeb14fe891deb08dbeb0760d6d5a03
Joel Day <joel.day@accretivetg.com>
2018-08-26 18:31:27 -0700

    Fix unzip, hopefully.

--- build/ci-pull-fo4.sh
+++ build/ci-pull-fo4.sh
@@ -7,7 +7,7 @@ echo Pulling Fallout 4 base scripts...
 mkdir papyrus/FO4Scripts
 cd papyrus/FO4Scripts
 
-curl -L -o Base.zip --silent https://www.dropbox.com/s/xw646rnme3o30pu/Base.zip?dl=1
-unzip -f -q Base.zip -d Base
+curl -L -o Base.zip https://www.dropbox.com/s/xw646rnme3o30pu/Base.zip?dl=1
+unzip -q Base.zip -d Base
 
 popd > /dev/null
