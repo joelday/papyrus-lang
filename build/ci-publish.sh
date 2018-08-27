9c685ce83b8c555ef41cb92262a3de5d4f851184
Joel Day <joel.day@accretivetg.com>
2018-08-26 17:26:36 -0700

    Published version check for VSCode extension.

--- build/ci-publish.sh
+++ build/ci-publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -z "$CI" ]; then
-    echo "Error: Publish script can only run in a CI environment."
-    exit 1
-fi
+# if [ -z "$CI" ]; then
+#     echo "Error: Publish script can only run in a CI environment."
+#     exit 1
+# fi
 
 set -e
 
@@ -20,7 +20,6 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
 lerna publish minor --yes $LERNA_PUBLISH_OPTIONS
 
-echo Publishing Visual Studio Code extension package...
 pushd ./ > /dev/null
 cd packages/papyrus-lang-vscode
 
@@ -28,7 +27,15 @@ rm -rf node_modules
 rm package-lock.json
 npm install --no-optional
 
-vsce package
-vsce publish --pat $VSCE_TOKEN
+published_vscode_ext_version=$(vsce show joelday.papyrus-lang-vscode --json | jq '.versions[0].version')
+current_vscode_ext_version=$(cat ./package.json | jq '.version')
+
+echo "Publishing Visual Studio Code extension package..."
+if [ $published_vscode_ext_version != $current_vscode_ext_version ]; then
+    vsce package
+    vsce publish --pat $VSCE_TOKEN
+else
+    echo "Current published version is up to date."
+fi
 
 popd > /dev/null
