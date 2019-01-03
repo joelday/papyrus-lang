1b31a9529e88b117b087ff938a46842f835ae232
Joel Day <joelday@gmail.com>
2019-01-02 23:14:52 -0800

    Now indicating that scripts are either overridden or unresolved.

--- src/papyrus-lang-vscode/src/extension.ts
+++ src/papyrus-lang-vscode/src/extension.ts
@@ -1,10 +1,64 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 import { ClientServer } from './ClientServer';
 import { SyntaxTreeDataProvider } from './SyntaxTreeDataProvider';
 import { SyntaxExplorerView } from './SyntaxExplorerView';
 
 let clientServer: ClientServer;
+
+async function getDocumentScriptStatus(document: vscode.TextDocument) {
+    const scriptInfo = await clientServer.requestScriptInfo(document.uri.toString());
+
+    const documentIsUnresolved = scriptInfo.identifiers.length === 0;
+    const documentIsOverridden =
+        !documentIsUnresolved &&
+        !scriptInfo.identifierFiles.some((identifierFile) =>
+            identifierFile.files.some((file) => file.toLowerCase() === document.uri.fsPath.toLowerCase())
+        );
+
+    return {
+        documentIsUnresolved,
+        documentIsOverridden,
+        scriptInfo,
+    };
+}
+
+function createZeroLens() {
+    return new vscode.CodeLens(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)));
+}
+
+class ScriptStatusCodeLensProvider implements vscode.CodeLensProvider {
+    async provideCodeLenses(document: vscode.TextDocument, _: vscode.CancellationToken): Promise<vscode.CodeLens[]> {
+        const info = await getDocumentScriptStatus(document);
+        const lenses: vscode.CodeLens[] = [];
+
+        if (info.documentIsUnresolved) {
+            const lens = createZeroLens();
+
+            lens.command = {
+                title: "Not included in a Papyrus project or CreationKit's configured source paths.",
+                command: '',
+            };
+
+            lenses.push(lens);
+        } else if (info.documentIsOverridden) {
+            const lens = createZeroLens();
+            const overridingFile = info.scriptInfo.identifierFiles[0].files[0];
+            const overridingFileUri = vscode.Uri.file(overridingFile);
+
+            lens.command = {
+                title: `Overridden by ${path.basename(overridingFile)}`,
+                command: 'vscode.open',
+                arguments: [overridingFileUri],
+            };
+
+            lenses.push(lens);
+        }
+
+        return lenses;
+    }
+}
 
 export async function activate(context: vscode.ExtensionContext) {
     const toolPath = context.asAbsolutePath('./bin/DarkId.Papyrus.Host.exe');
@@ -23,14 +77,42 @@ export async function activate(context: vscode.ExtensionContext) {
         syntaxExplorer.register();
     }
 
+    var overriddenOrInactiveDecoration = vscode.window.createTextEditorDecorationType({
+        opacity: '0.5',
+    });
+
     vscode.window.onDidChangeActiveTextEditor(async (e) => {
         if (e.document.languageId !== 'papyrus') {
             return;
         }
 
-        var info = await clientServer.requestScriptInfo(e.document.uri.toString());
-        console.log(info);
+        try {
+            const { documentIsUnresolved, documentIsOverridden } = await getDocumentScriptStatus(e.document);
+
+            if (documentIsUnresolved || documentIsOverridden) {
+                e.setDecorations(overriddenOrInactiveDecoration, [
+                    {
+                        range: new vscode.Range(
+                            new vscode.Position(0, 0),
+                            e.document.positionAt(e.document.getText().length)
+                        ),
+                    },
+                ]);
+            } else {
+                e.setDecorations(overriddenOrInactiveDecoration, []);
+            }
+        } catch {
+            e.setDecorations(overriddenOrInactiveDecoration, []);
+        }
     });
+
+    vscode.languages.registerCodeLensProvider(
+        {
+            language: 'papyrus',
+            scheme: 'file',
+        },
+        new ScriptStatusCodeLensProvider()
+    );
 }
 
 export function deactivate() {
