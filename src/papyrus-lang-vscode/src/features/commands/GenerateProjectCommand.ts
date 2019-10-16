afc3a9befd25fc3b48f5abe44ad9431006cb7cb7
rjstone <rjstoneus@gmail.com>
2019-10-16 01:45:12 -0400

    Add context menu items and fix command class

--- src/papyrus-lang-vscode/src/features/commands/GenerateProjectCommand.ts
+++ src/papyrus-lang-vscode/src/features/commands/GenerateProjectCommand.ts
@@ -1,13 +1,17 @@
+import { Disposable } from 'vscode';
 import { GameCommandBase } from './GameCommandBase';
 import { PapyrusGame } from '../../PapyrusGame';
 
-export class GenerateProjectCommand extends GameCommandBase {
+export class GenerateProjectCommand extends GameCommandBase<[string]> {
 
-    constructor(name: string) {
-        super(name, [PapyrusGame.skyrimSpecialEdition]); // pass additional args for execute() and onExecute() here
+    constructor() {
+        super("generateProject"); // pass additional args for execute() and onExecute() here
     }
 
-    protected async onExecute() {
-
+    protected async onExecute(game: PapyrusGame, ...args: [string]) {
+        console.log("GenerateProjectCommand: game=" + game + "\narg0=" + args.join('\nargX='));
     }
+
+    //    public async execute(game: PapyrusGame, ...args:[any]) {
+    //    }
 }
