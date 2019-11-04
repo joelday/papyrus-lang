eb98b39ba35d876d66a6de7d39a6349f5fbf778b
rjstone <rjstone@radix.cc>
2019-11-03 22:20:36 -0500

    getGameIsRunning went missing on refactoring add it back. Also add getWorkspaceGame()

--- src/papyrus-lang-vscode/src/Utilities.ts
+++ src/papyrus-lang-vscode/src/Utilities.ts
@@ -2,7 +2,11 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { promisify } from 'util';
 
-import { CancellationTokenSource } from 'vscode';
+import procList from 'ps-list';
+
+import { workspace, CancellationTokenSource } from 'vscode';
+import { PapyrusGame } from './PapyrusGame';
+import { getExecutableNameForGame } from './Paths';
 
 
 const exists = promisify(fs.exists);
@@ -18,6 +22,11 @@ export function* flatten<T>(arrs: T[][]): IterableIterator<T> {
 
 export function delayAsync(durationMs: number): Promise<void> {
     return new Promise((r) => setTimeout(r, durationMs));
+}
+
+export async function getGameIsRunning(game: PapyrusGame) {
+    const processList = await procList();
+    return processList.some((p) => p.name.toLowerCase() === getExecutableNameForGame(game).toLowerCase());
 }
 
 export async function waitWhile(
@@ -87,4 +96,28 @@ export function mkDirByPathSync(targetDir: string, { isRelativeToScript = false 
 
         return curDir;
     }, initDir);
+}
+
+export function getWorkspaceGame(): PapyrusGame | undefined {
+    let game = undefined;
+    const workspaceRoot = workspace.workspaceFolders[0].uri.fsPath;
+
+    // If Sources/Scripts exists then it's a Skyrim of some sort
+    if (exists(path.join(workspaceRoot, 'Sources', 'Scripts'))) {
+        // If the executable in the directory above is Skyrim.exe then we assume it's classic.
+        if (exists(path.join(workspaceRoot, '..', getExecutableNameForGame(PapyrusGame.skyrim)))) {
+            game = PapyrusGame.skyrim;
+        } else {
+            // otherwise we assume it's Special Edition
+            game = PapyrusGame.skyrimSpecialEdition;
+        }
+    } else {
+        // If the Fallout 4 Scripts/Sources/User exists then we assme it's Fallout 4
+        if (exists(path.join(workspaceRoot, 'Scripts', 'Sources', 'User'))) {
+            game = PapyrusGame.fallout4;
+        }
+        // Otherwise it's something we can't identify.
+    }
+
+    return game;
 }
