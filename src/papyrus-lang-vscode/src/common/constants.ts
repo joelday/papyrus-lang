e31f825cb36003084589bc6f1ccf57611a94297b
rjstone <rjstone@radix.cc>
2019-11-10 20:11:29 -0500

    Add global constants file
    This is for any sort of miscellaneous global constants.

---
+++ src/papyrus-lang-vscode/src/common/constants.ts
@@ -0,0 +1,9 @@
+// Constants for use elsewhere in the code.
+// We shoud slowly start moving things in here.
+
+export const extensionId = 'papyrus-lang';
+export const extensionQualifiedId = `joelday.${extensionId}`;
+
+export enum GlobalState {
+    PapyrusVersion = 'papyrusVersion'
+}
