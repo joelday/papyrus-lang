88490d4686e1f88c53a72a17f877ebe9f7a00e74
rjstone <rjstone@radix.cc>
2019-11-10 21:51:27 -0500

    add WelcomeHandler
    At some point we might also want to show a "what's new" when
    an upgrade happens. This is where the code for handling that
    will go. For now it doesn't really do much.

--- src/papyrus-lang-vscode/src/common/constants.ts
+++ src/papyrus-lang-vscode/src/common/constants.ts
@@ -1,7 +1,7 @@
 // Constants for use elsewhere in the code.
-// We shoud slowly start moving things in here.
+// We shoud slowly start moving some things in here.
 
-export const extensionId = 'papyrus-lang';
+export const extensionId = 'papyrus-lang-vscode';
 export const extensionQualifiedId = `joelday.${extensionId}`;
 
 export enum GlobalState {
