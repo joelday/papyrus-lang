de5ada526ae4ae2623c424a72dcad2f7da3e17b0
rjstone <rjstone@radix.cc>
2019-11-10 21:52:12 -0500

    add ShowWelcomeCommand (markdown)
    Add a command to show a markdown preview of our
    welcome and getting started document.

---
+++ src/papyrus-lang-vscode/src/features/commands/ShowWelcomeCommand.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+import { Uri, commands, window, ExtensionContext } from 'vscode';
+import { CommandBase } from '../../common/vscode/commands/CommandBase';
+import { IExtensionContext } from '../../common/vscode/IocDecorators';
+import { getWelcomeFile } from '../../Paths';
+
+export class ShowWelcomeCommand extends CommandBase<[Uri]> {
+    private readonly _context: ExtensionContext;
+    constructor(
+        @IExtensionContext context: ExtensionContext
+    ) {
+        super('papyrus.showWelcome');
+        this._context = context;
+    }
+
+    protected async onExecute() {
+        const fileUri = Uri.file(
+            path.join(this._context.extensionPath, getWelcomeFile())
+        );
+
+        commands.executeCommand('markdown.showPreview', fileUri, { locked: false });
+    }
+}
