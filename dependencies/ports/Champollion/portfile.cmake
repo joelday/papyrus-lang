86b2e1b63347d96fe9d8a0b1c91c5d168d49388b
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-08-31 23:39:24 -0700

    DebugServer: update dependencies

--- dependencies/ports/Champollion/portfile.cmake
+++ dependencies/ports/Champollion/portfile.cmake
@@ -1,28 +1,31 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO Orvid/Champollion
-  REF v1.1.0
-  SHA512 a95a2f82f27da2ac71fed156e32b824ef2897400c64cf3fd2221a367b61a6cc5bd224706868a9dc8ddd5f6c0054a10943d759cc170bebadca42a58384f8d0eef
+  REF 83d7f92481c797f2732fcaed016c24f9fcfb3eba
+  SHA512 2ffd56fc9f5ec98444124d60e86771e7e5aa1f68d27bd561bcffe0d5b04ac98913e656a50131a28178d39231404bb2502737bf56c07592867b871f372fcdce95
   HEAD_REF master
 )
+
 # Check if one or more features are a part of a package installation.
 # See /docs/maintainers/vcpkg_check_features.md for more details
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
   INVERTED_FEATURES
-	  standalone   CHAMPOLLION_STATIC_LIBRARY
+  standalone CHAMPOLLION_STATIC_LIBRARY
 )
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-		OPTIONS
-      ${FEATURE_OPTIONS}
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+  ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
-if (${CHAMPOLLION_STATIC_LIBRARY})
+
+if(${CHAMPOLLION_STATIC_LIBRARY})
   vcpkg_cmake_config_fixup(PACKAGE_NAME Champollion CONFIG_PATH lib/cmake/Champollion)
 endif()
+
 file(
-        INSTALL "${SOURCE_PATH}/LICENSE"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-        RENAME copyright)
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
--- dependencies/ports/commonlibf4/portfile.cmake
+++ dependencies/ports/commonlibf4/portfile.cmake
@@ -1,57 +1,57 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO nikitalita/CommonLibF4
-  REF 71da8c1d96b6084795a61dce4b453583396f240b
-  SHA512 9b0c4a7ef09b7fd4362dce16cfd4bdc57b077498d534ccf7b52a28916ecafe913f24281668323612c52a558d7dc829e8840aea771f4c7818e56b2f0876deddf2
+  REPO powerof3/CommonLibF4
+  REF d3e9231e796eb5dd4b7dd3d1d778e8c1807ffc18
+  SHA512 e1ddb0cc04ee9eecba3a8e69d56bb0e6190fbe92db12f51cc1aff5c7400e4ff980cf365dfca2d3bebe5681a85925639dc33ce769bc6e6ee4da83b2b400a78476
   HEAD_REF master
 )
-     
-# set(SOURCE_PATH "C:\\Users\\Nikita\\Workspace\\skyrim-mod-workspace\\CommonLibF4-merge-ws\\CommonLibF4")
 
-if ( VCPKG_HOST_IS_WINDOWS AND VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW AND DEFINED $ENV{VisualStudioVersion} )
-  set( MSVC_RUNTIME_VCPKG_CONFIG_ARG "-DCMAKE_MSVC_RUNTIME_LIBRARY=\"MultiThreaded$<$<CONFIG:Debug>:Debug>DLL\"")
+# set(SOURCE_PATH "C:\\Users\\Nikita\\Workspace\\skyrim-mod-workspace\\CommonLibF4-merge-ws\\CommonLibF4")
+if(VCPKG_HOST_IS_WINDOWS AND VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW AND DEFINED $ENV{VisualStudioVersion})
+  set(MSVC_RUNTIME_VCPKG_CONFIG_ARG "-DCMAKE_MSVC_RUNTIME_LIBRARY=\"MultiThreaded$<$<CONFIG:Debug>:Debug>DLL\"")
+
   # 2022 requires `/external:anglebrackets` to build with the above argument
-  if ( $ENV{VisualStudioVersion} STREQUAL "17.0")
-    set( CXX_FLAGS_VCPKG_CONFIG_ARG "-DCMAKE_CXX_FLAGS=\"/EHsc /MP /W4 /WX /external:anglebrackets /external:W0\"")
+  if($ENV{VisualStudioVersion} STREQUAL "17.0")
+    set(CXX_FLAGS_VCPKG_CONFIG_ARG "-DCMAKE_CXX_FLAGS=\"/EHsc /MP /W4 /WX /external:anglebrackets /external:W0\"")
   else()
-    set( CXX_FLAGS_VCPKG_CONFIG_ARG "-DCMAKE_CXX_FLAGS=\"/EHsc /MP /W4 /WX /external:W0\"")
-  endif()  
+    set(CXX_FLAGS_VCPKG_CONFIG_ARG "-DCMAKE_CXX_FLAGS=\"/EHsc /MP /W4 /WX /external:W0\"")
+  endif()
 else()
-  set( MSVC_RUNTIME_VCPKG_CONFIG_ARG "")
-  set( CXX_FLAGS_VCPKG_CONFIG_ARG "")
-endif()  
+  set(MSVC_RUNTIME_VCPKG_CONFIG_ARG "")
+  set(CXX_FLAGS_VCPKG_CONFIG_ARG "")
+endif()
 
 # set(MSVC_RUNTIME_ARG "Some Text")
 vcpkg_configure_cmake(
   SOURCE_PATH "${SOURCE_PATH}"
   PREFER_NINJA
   OPTIONS
-    -DVCPKG_OVERLAY_PORTS="${SOURCE_PATH}/cmake/ports/"
-    -DBoost_USE_STATIC_LIBS=ON
-    -DBoost_USE_STATIC_RUNTIME=OFF
-    -DBoost_USE_STATIC_LIBS=ON
-    -DF4SE_SUPPORT_XBYAK=ON
-    -DCMAKE_EXE_LINKER_FLAGS_RELEASE="/DEBUG:FASTLINK"
-    ${MSVC_RUNTIME_VCPKG_CONFIG_ARG}
-    ${CXX_FLAGS_VCPKG_CONFIG_ARG}
+  -DVCPKG_OVERLAY_PORTS="${SOURCE_PATH}/cmake/ports/"
+  -DBoost_USE_STATIC_LIBS=ON
+  -DBoost_USE_STATIC_RUNTIME=OFF
+  -DBoost_USE_STATIC_LIBS=ON
+  -DF4SE_SUPPORT_XBYAK=ON
+  -DCMAKE_EXE_LINKER_FLAGS_RELEASE="/DEBUG:FASTLINK"
+  ${MSVC_RUNTIME_VCPKG_CONFIG_ARG}
+  ${CXX_FLAGS_VCPKG_CONFIG_ARG}
 )
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(     
-  PACKAGE_NAME CommonLibF4   
-  CONFIG_PATH "lib/cmake/CommonLibF4"   
+vcpkg_cmake_config_fixup(
+  PACKAGE_NAME CommonLibF4
+  CONFIG_PATH "lib/cmake/CommonLibF4"
 )
 file(GLOB CMAKE_CONFIGS "${CURRENT_PACKAGES_DIR}/share/CommonLibF4/CommonLibF4/*.cmake")
 file(INSTALL ${CMAKE_CONFIGS} DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibF4")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/CommonLibF4/CommonLibF4")
-# file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include/CommonLibF4")  
+
+# file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include/CommonLibF4")
 # file(RENAME "${CURRENT_PACKAGES_DIR}/include/F4SE" "${CURRENT_PACKAGES_DIR}/include/CommonLibF4/F4SE")
 # file(RENAME "${CURRENT_PACKAGES_DIR}/include/RE" "${CURRENT_PACKAGES_DIR}/include/CommonLibF4/RE")
 # file(RENAME "${CURRENT_PACKAGES_DIR}/include/REL" "${CURRENT_PACKAGES_DIR}/include/CommonLibF4/REL")
-
 file(
   INSTALL "${SOURCE_PATH}/LICENSE"
   DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
